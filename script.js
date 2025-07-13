33e5bef27a0cfa723bcb62cfa86a017869a49e11
salon-a11y <manshimaurya195@gmail.com>
2025-07-13 15:15:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 // Complete the js code
 function Car(make, model) {}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed){}
+class Car {
+	constructor(M,m) {
+		this.make = M
+		this.model = m
+		
+	}
+}
+class Sportcar extends Car {
+	constructor(M,m,t) {
+		super(M,m)
+		this.topspeed = t
+		
+	}
+}
+let Sportcar1 = new SportsCar("farrari","testrarosa",200)
+console.log(Sportcar1)
 
-// Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
